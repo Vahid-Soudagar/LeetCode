3e9fc0cec7a5b0506d03a3b930ef998d4e2153a5
Vahid-Soudagar <88474649+Vahid-Soudagar@users.noreply.github.com>
2023-11-09 13:18:58 +0530

    Time: 6 ms (98.80%) | Memory: 44.4 MB (20.00%) - LeetSync

--- 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.java
+++ 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.java
@@ -27,3 +27,37 @@ class Solution {
         return (int)(res%(Math.pow(10,9)+7));
     }
 }
+
+/*
+class Solution {
+    public int countHomogenous(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int mod = 1_000_000_007;
+        int sp = 0;
+        int ep = 0;
+        int count = 0;
+        while (ep < s.length()) {
+            char ch = s.charAt(ep);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            while (!isHomogenous(map)) {
+                char spChar = s.charAt(sp);
+                int freq = map.get(spChar);
+                if (freq == 1) {
+                    map.remove(spChar);
+                } else {
+                    map.put(spChar, freq - 1);
+                }
+                sp++;
+            }
+            count = count + (ep - sp + 1);
+            ep++;
+        }
+        return count%mod;
+    }
+
+    public boolean isHomogenous(HashMap<Character, Integer> map) {
+        return map.size() == 1 || map.isEmpty();
+    }
+}
+
+*/
