5bc458860da28c54fffe34e5b884cf7753657767
Vahid-Soudagar <88474649+Vahid-Soudagar@users.noreply.github.com>
2023-11-15 12:48:05 +0530

    Time: 7 ms (45.71%) | Memory: 57.2 MB (10.71%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.java
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.java
@@ -67,21 +67,28 @@ class Solution {
         int n = arr.length;
         Arrays.sort(arr);
 
-        if (arr[0] == 1) {
-            for (int i=1;i<n;i++) {
-                if (Math.abs(arr[i] - arr[i-1]) > 1) {
-                    arr[i] = arr[i-1]+1;
-                } 
-            }
-        } else {
-            arr[0] = 1;
-            for (int i=1;i<n;i++) {
-                if (Math.abs(arr[i] - arr[i-1]) > 1) {
-                    arr[i] = arr[i-1]+1;
-                } 
-            }
+        // if (arr[0] == 1) {
+        //     for (int i=1;i<n;i++) {
+        //         if (Math.abs(arr[i] - arr[i-1]) > 1) {
+        //             arr[i] = arr[i-1]+1;
+        //         } 
+        //     }
+        // } else {
+        //     arr[0] = 1;
+        //     for (int i=1;i<n;i++) {
+        //         if (Math.abs(arr[i] - arr[i-1]) > 1) {
+        //             arr[i] = arr[i-1]+1;
+        //         } 
+        //     }
+        // }
+
+        arr[0] = 1;
+        for (int i=1;i<n;i++) {
+            if (Math.abs(arr[i] - arr[i-1]) > 1) {
+                arr[i] = arr[i-1]+1;
+            } 
         }
 
         return arr[n-1];
     }
-}
+} 
