590cfc17d202f69aa3051c042db1a182d6322150
Vahid-Soudagar <88474649+Vahid-Soudagar@users.noreply.github.com>
2023-11-25 11:18:56 +0530

    Time: 4 ms (65.38%) | Memory: 59.6 MB (14.74%) - LeetSync

--- 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.java
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int[] getSumAbsoluteDifferences(int[] nums) {
         int n = nums.length;
         int[] res = new int[n];
@@ -17,5 +17,21 @@ class Solution {
         }
 
         return res;
+    }
+} */
+
+class Solution {
+    public int[] getSumAbsoluteDifferences(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        int temp=0;
+        for(int i=0 ; i<n ; i++){
+            temp += Math.abs(nums[i]-nums[0]);
+        }
+        ans[0]=temp;
+        for(int i=1; i<n; i++){
+            ans[i] = ans[i-1]+(nums[i]-nums[i-1])*i - (nums[i]-nums[i-1])*(n-i);  
+        }
+        return ans;
     }
 }
