dff163cdf8e9984380534f682231dd5fc655d600
Vahid-Soudagar <88474649+Vahid-Soudagar@users.noreply.github.com>
2024-06-13 01:18:43 +0530

    Time: 1 ms (76.44%), Space: 40.5 MB (74.82%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -2,10 +2,14 @@ class Solution {
     public int reverse(int x) {
       long sum=0;
       int k=x;
+    //   -123
       if(x<0)
         k=(-1)*k;
+        // -1 * -123 = 123;
+
       while(k>0) {
         sum=sum*10+x%10;
+        // 0 * 10 + -123 % 10 = 0 
         x=x/10;
         k=k/10;
       }
